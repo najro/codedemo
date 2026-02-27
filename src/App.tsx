4f52ab4c9f8da96decf7899d599df11eb740984c
Örjan Andersson <orjan@najro.com>
2026-02-27 20:23:23 +0100

    Add start page component for today's date

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { TodayDate } from './components/TodayDate'
+
 export function App() {
   return (
     <main className="app">
       <h1>Hello, world! 👋</h1>
       <p>Your React + TypeScript app is ready.</p>
+      <TodayDate />
     </main>
   )
 }
