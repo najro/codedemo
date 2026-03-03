29fa951d2ef83b0847cf5ca989cb4c482c6aceca
Suncher1 <magnus@suncher.se>
2026-03-03 21:31:41 +0100

    Initial React Application

---
+++ hello-world-react/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-slate-900">Hello World!</h1>
+      <h2>This is a title</h2>
+    </main>
+  );
+}
